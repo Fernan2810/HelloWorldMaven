6b18f385662d25aa10083f3e72fa304a49c1b0a1
Fernan2810 <tb25004@ues.edu.sv>
2025-05-11 21:37:04 -0600

    My first commit

---
+++ demo-tb25004/src/main/java/com/iis/tb25004/App.java
@@ -0,0 +1,14 @@
+package com.iis.tb25004;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
